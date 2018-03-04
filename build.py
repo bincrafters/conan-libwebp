f932f1a8cb473078b0e27783c09dc04c64f41ebb
theirix <theirix@gmail.com>
2018-03-05 01:34:27 +0300

    Skip mingw cross-builds

--- build.py
+++ build.py
@@ -3,9 +3,18 @@
 
 
 from bincrafters import build_template_default
+import platform
 
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
+
+    items = []
+    for item in builder.items:
+        # skip mingw cross-builds
+        if not (platform.system() == "Windows" and item.settings["compiler"] == "gcc" and
+                item.settings["arch"] == "x86"):
+            items.append(item)
+    builder.items = items
 
     builder.run()
